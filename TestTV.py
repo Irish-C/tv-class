36d89eb4e5f47810818e8a321a809d4c61f12b0a
Irish-C <maryirish.cammagay@gmail.com>
2023-05-15 16:13:32 +0800

    delete testtv layout, make TestTV class

--- TestTV.py
+++ TestTV.py
@@ -5,20 +5,7 @@
 from TV import TV
 import PySimpleGUI as sg
 
-# Create TestTV window's content
-sg.theme('DarkTeal4')
 
-TestTV_layout = [
-    [sg.Text('TV1', justification='center')],
-    [sg.Text('Change channel'),sg.InputText(key='channel_1', size=(2,1)), sg.Text('Adjust volume'), sg.InputText(key='vol_1', size=(2,1))],
-    [sg.Text('TV2', justification='center')],
-    [sg.Text('Change channel'),sg.InputText(key='channel_2', size=(2,1)), sg.Text('Adjust volume'), sg.InputText(key='vol_2', size=(2,1))],
-    [sg.Text('')],  #Creates an empty row
-    [sg.Button('OK')]
-
-]
-
-# Create TestTV window
-TestTV_window = sg.Window('Test Driver Program', TestTV_layout)
-
-event, values = TestTV_window.read()
+class TestTV:
+    # Create TestTV window's content
+    sg.theme('DarkTeal4')
