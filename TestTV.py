c6192b39f1fc2d1233f8eddb307c7ffbe26cb736
Irish-C <maryirish.cammagay@gmail.com>
2023-05-15 16:33:55 +0800

    Add read/close window and FIX TestTV layout

--- TestTV.py
+++ TestTV.py
@@ -24,13 +24,15 @@ def TestTV():
     tv2.setVolume(2)
 
     TestTV_layout = [
-        [sg.Frame('TV1'), [
-            [sg.Text(f"TV1's channel is {tv1.getChannel()} and volume level is {tv1.getVolume()}")],]],
-        [sg.Frame('TV2'), [
-            [sg.Text(f"TV2's channel is {tv2.getChannel()} and volume level is {tv2.getVolume()}")],]]
+        [sg.Frame('TV1', [[sg.Text(f"TV1's channel is {tv1.getChannel()} and volume level is {tv1.getVolume()}")]])],
+        [sg.Frame('TV2', [[sg.Text(f"TV2's channel is {tv2.getChannel()} and volume level is {tv2.getVolume()}")]])]
     ]
 
     # Create TestTV window
     TestTV_window = sg.Window('Test Driver Program', TestTV_layout)
     
     TestTV_window.read()
+    TestTV_window.close()
+
+if __name__ == '__main__':
+    TestTV()
