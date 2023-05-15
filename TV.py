83e10d284acc0f65b365981b80b9687d236d1ce7
Irish-C <maryirish.cammagay@gmail.com>
2023-05-15 12:34:16 +0800

    construct init method of TV class

--- TV.py
+++ TV.py
@@ -1,1 +1,7 @@
-# TV class
+# Define TV class
+class TV:
+    # constructor init method for properties of an TV object
+    def __init__(self):
+        self.channel = 1
+        self.volumeLevel = 1
+        self.on = False
