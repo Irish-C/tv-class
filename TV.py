698ae348c43ead7fcb5d9c24149a46f640740470
Irish-C <maryirish.cammagay@gmail.com>
2023-05-15 12:53:39 +0800

    add comment for values in init method

--- TV.py
+++ TV.py
@@ -2,9 +2,9 @@
 class TV:
     # constructor init method for properties of an TV object
     def __init__(self):
-        self.channel = 1
-        self.volumeLevel = 1
-        self.on = False
+        self.channel = 1        # sets initial channel to 1
+        self.volumeLevel = 1    # sets initial volume to 1
+        self.on = False         # sets initial on to False if TV is initially turned off
     
 
     # function that returns True when TV is on
