c9c842175c7cd634968e094163307dbefdf54de2
Irish-C <maryirish.cammagay@gmail.com>
2023-05-15 15:06:03 +0800

    create TestTV window

--- TestTV.py
+++ TestTV.py
@@ -1,2 +1,20 @@
 # Test Driver Program
-# Create two objects from class TV
+
+
+# import modules
+from TV import TV
+import PySimpleGUI as sg
+
+# Create TestTV window's content
+sg.theme('DarkTeal4')
+
+TestTV_layout = [
+    [sg.Text('TV1', justification='center')],
+]
+
+# Create TestTV window
+TestTV_window = sg.Window('Test Driver Program', TestTV_layout)
+
+event, values = TestTV_window.read()
+
+TestTV_window.close()
