1437d6bfa1b463c37889a2c26fc44ba59cc7a370
Irish-C <maryirish.cammagay@gmail.com>
2023-05-15 13:19:30 +0800

    rewrite a better comment

--- TV.py
+++ TV.py
@@ -1,20 +1,20 @@
 # Define TV class
 class TV:
-    # constructor init method for properties of an TV object
+    # constructor attributes of an TV object
     def __init__(self):
         self.channel = 1        # sets initial channel to 1
         self.volumeLevel = 1    # sets initial volume to 1
         self.on = False         # sets initial on to False if TV is initially turned off
     
 
-    # function that returns True when TV is on
+    # a method that returns True when TV is on
     def turnOn(self):
         self.on = True
 
-    # function that return False when TV is off
+    # a method that return False when TV is off
     def turnOff(self):
         self.on = False
     
-    # function that returns the channel
+    # a method that returns the channel
     def getChannel(self):
         return self.channel
