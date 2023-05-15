f0f35fa8ade3f542261b628ac00a8cee0919dbd1
Irish-C <maryirish.cammagay@gmail.com>
2023-05-15 16:21:16 +0800

    change from class to function

--- TestTV.py
+++ TestTV.py
@@ -6,6 +6,19 @@ from TV import TV
 import PySimpleGUI as sg
 
 
-class TestTV:
-    # Create TestTV window's content
-    sg.theme('DarkTeal4')
+# Define TestTV
+def TestTV():
+
+    # Create two objects
+    tv1 = TV()
+    tv2 = TV()
+
+    # turn on tv1 and set its channel t0 30 and volume to 3
+    tv1.turnOn()
+    tv1.setChannel(30)
+    tv1.setVolume(3)
+
+    # Turn on tv2 and sets its channel to 3 and volume to 2
+    tv2.turnOn()
+    tv2.setChannel(3)
+    tv2.setVolume(2)
